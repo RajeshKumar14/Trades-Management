46b52a3b64b6af41b28656fb84b5defec1c9bf38
Rajesh Kumar <rajesh.kumar@cred.club>
2020-11-28 21:45:10 +0530

    add schema and TradeData.json

--- src/test/java/com/simpragma/TradesManagement/TradesManagementApplicationTests.java
+++ src/test/java/com/simpragma/TradesManagement/TradesManagementApplicationTests.java
@@ -1,8 +1,15 @@
 package com.simpragma.TradesManagement;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import lombok.extern.slf4j.Slf4j;
 
+@Ignore
+@Slf4j
+@RunWith (SpringRunner.class)
 @SpringBootTest
 class TradesManagementApplicationTests {
 
